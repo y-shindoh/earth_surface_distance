3e2d775fbdc2bd637fa6a312aa7b5c4ec7a8d9a0
Yasutaka SHINDOH <cube@quruli.ivory.ne.jp>
2015-03-06 21:35:18 +0900

    Modify class and module.

--- earth_surface_distance.rb
+++ earth_surface_distance.rb
@@ -2,27 +2,33 @@
 # -*- coding: utf-8; tab-width: 4 -*-
 # Author: Yasutaka SHINDOH
 
-class EarthSurfaceDistance
+class EarthSurfacePoint
     attr_reader :name, :latitude, :longitude
-
-    LONG_RADIUS = 6378137.0			# 赤道半径 (WGS84)
-    SHORT_RADIUS = 6356752.314245	# 極半径 (WGS84)
-    ECCENTRICITY = Math::sqrt(((LONG_RADIUS ** 2) - (SHORT_RADIUS ** 2)) / (LONG_RADIUS ** 2))
 
     def initialize(name, latitude, longitude)
         @name = name	# 地名
-        @latitude = latitude / 360.0 * 2.0 * Math::PI	# 緯度 (ラジアン)
-        @longitude = longitude / 360.0 * 2.0 * Math::PI	# 経度 (ラジアン)
+        @latitude = EarthSurfacePoint::Angle2Radian(latitude)	# 緯度
+        @longitude = EarthSurfacePoint::Angle2Radian(longitude)	# 経度
     end
 
     def initialize(name, string)
         terms = string.split(/\s*,\s*/)
         @name = name	# 地名
-        @latitude = terms[0].to_f / 360.0 * 2.0 * Math::PI	# 緯度 (ラジアン)
-        @longitude = terms[1].to_f / 360.0 * 2.0 * Math::PI	# 経度 (ラジアン)
+        @latitude = EarthSurfacePoint::Angle2Radian(terms[0].to_f)	# 緯度
+        @longitude = EarthSurfacePoint::Angle2Radian(terms[1].to_f)	# 経度
     end
 
-    def EarthSurfaceDistance::Get(from, to)
+    def EarthSurfacePoint::Angle2Radian(value)
+        value / 360.0 * 2.0 * Math::PI
+    end
+end
+
+module EarthSurfaceDistance
+    LONG_RADIUS = 6378137.0			# 赤道半径 (WGS84)
+    SHORT_RADIUS = 6356752.314245	# 極半径 (WGS84)
+    ECCENTRICITY = Math::sqrt(((LONG_RADIUS ** 2) - (SHORT_RADIUS ** 2)) / (LONG_RADIUS ** 2))
+
+    def EarthSurfaceDistance::Calculate(from, to)
         # 緯度・経度の差
         delta_latitude = from.latitude - to.latitude
         delta_longitude = from.longitude - to.longitude
@@ -47,9 +53,9 @@ if __FILE__ == $PROGRAM_NAME
     ARGV.each { |v|
         if v.include?(':') then
             term = v.split(/\s*:\s*/)
-            buffer << EarthSurfaceDistance::new(term[0], term[1])
+            buffer << EarthSurfacePoint::new(term[0], term[1])
         else
-            buffer << EarthSurfaceDistance::new(buffer.length.to_s, v)
+            buffer << EarthSurfacePoint::new(buffer.length.to_s, v)
         end
     }
 
@@ -58,7 +64,7 @@ if __FILE__ == $PROGRAM_NAME
     m = buffer.length - 1
     m.times { |i|
         j = i + 1
-        d = EarthSurfaceDistance::Get(buffer[i], buffer[j])
+        d = EarthSurfaceDistance.Calculate(buffer[i], buffer[j])
         printf("[%s => %s]\t%f\n", buffer[i].name, buffer[j].name, d)
         total += d
     }
